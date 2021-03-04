5c7765171621c0e064df6bd6e241af0b157feb9f
kaursukhvir <kaursukh61324@gmail.com>
2021-03-04 14:01:31 +0530

    second commit

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const svgIcon=document.querySelectorAll('svg');
 const person=document.querySelectorAll('.person');
 
+function nextClick(){
+    person[1].classList.remove('display');
+    person[0].classList.add('display');
+}
 
-svgIcon[0].addEventListener('click',(e)=>{
+function prevClick(){
     person[0].classList.remove('display');
     person[1].classList.add('display');
-})
+}
 
-svgIcon[2].addEventListener('click',(e)=>{
-    person[0].classList.toggle('display');
-    person[1].classList.add('display');
-})
+svgIcon[0].addEventListener('click',prevClick)
+svgIcon[2].addEventListener('click',prevClick)
+svgIcon[1].addEventListener('click',nextClick)
+svgIcon[3].addEventListener('click',nextClick)
 
-svgIcon[1].addEventListener('click',(e)=>{
-    person[1].classList.remove('display');
-    person[0].classList.add('display');
-})
-
-svgIcon[3].addEventListener('click',(e)=>{
-    person[1].classList.remove('display');
-    person[0].classList.add('display');
-})
-
+document.onkeydown = function (e){
+    switch(e.key){
+        case 'ArrowRight':
+            nextClick();
+            break;
+        case 'ArrowLeft':
+            prevClick();
+            break;
+    }
+}
